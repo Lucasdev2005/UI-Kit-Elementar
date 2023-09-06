e716eafc934d5e3760320b046deaa6174636de05
Lucas Moreira Nunes <lucasdev2005@gmail.com>
2023-09-06 20:04:20 -0300

    Update package.json and Input.tsx

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { FormGroup } from "./InputStyle"
+import { FormGroup } from "./InputStyle";
 
 export interface InputProps {
     type: "text" | "number" | "email" | "password",
     placeHolder: string,
     setModel: (event: any) => any,
     disabled?: boolean,
-    hexadecimal: string;
+    hexadecimal: string
 }
 
 export function Input({type, placeHolder , setModel, disabled, hexadecimal}: InputProps) {
     return (
-        <FormGroup color={hexadecimal}>
+        <FormGroup color={'#'+ hexadecimal}>
             <input onChange={event => setModel(event.target.value)} type={type} className="form__field" placeholder={placeHolder} disabled={disabled} />
             <label htmlFor={placeHolder} className="form__label">{placeHolder}</label>
         </FormGroup>
