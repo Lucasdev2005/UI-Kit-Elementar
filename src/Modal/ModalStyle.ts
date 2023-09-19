4fe0617d6d4d6c5f0e74a99775a0b66bc8bc5c9d
Lucas Moreira Nunes <lucasdev2005@gmail.com>
2023-09-18 21:50:11 -0300

    Update package.json, Modal.tsx, and ModalStyle.ts

--- src/Modal/ModalStyle.ts
+++ src/Modal/ModalStyle.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const ModalStyle = styled.div`
-    padding: 5%;
     z-index: 9999;
     position: fixed;
     left: 0;
@@ -9,14 +8,15 @@ export const ModalStyle = styled.div`
     width: 100%;
     height: 100%;
     overflow: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: rgb(0,0,0);
     background-color: rgba(0,0,0,0.4); 
 
     .modal-content {
         background-color: #fefefe;
-        margin: 15% auto;
-        padding: 20px;
-        border: 1px solid #888;
+        max-width: 80%;
         width: 80%;
     }
 
